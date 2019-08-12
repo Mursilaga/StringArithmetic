266b3bea34fda87c2d6c18ab06540cf477ead15e
Maksim.Baranov <max3nix@gmail.com>
2019-08-12 09:51:03 +0300

    subtract and isNegative functions

--- StringArithmetic/StringCalcInteger.h
+++ StringArithmetic/StringCalcInteger.h
@@ -17,7 +17,7 @@ public:
     }
 
 
-    //return true if a > b, otherwise retun false
+    //return true if a > b, otherwise return false
     static bool more(std::string a, std::string b) {
         deleteLeadingZeros(a);
         deleteLeadingZeros(b);
@@ -34,25 +34,26 @@ public:
         return false;
     }
 
-    //return true if a >= b, otherwise retun false
+    //return true if a >= b, otherwise return false
     static bool more_or_eq(std::string a, std::string b) {
         deleteLeadingZeros(a);
         deleteLeadingZeros(b);
         return more(a, b) || a == b;
     }
 
-    //return true if a < b, otherwise retun false
+    //return true if a < b, otherwise return false
     static bool less(std::string a, std::string b) {
         return more(b, a);
     }
 
-    //return true if a <= b, otherwise retun false
+    //return true if a <= b, otherwise return false
     static bool less_or_eq(std::string a, std::string b) {
         deleteLeadingZeros(a);
         deleteLeadingZeros(b);
         return less(a, b) || a == b;
     }
 
+    //return a + b
     static std::string add(std::string a, std::string b) {
         std::string res = "";
 
@@ -89,6 +90,62 @@ public:
 
         deleteLeadingZeros(res);
         return res;
+    }
+
+    //return a - b
+    static std::string subtract(std::string a, std::string b) {
+        if (a == b)
+            return "0";
+
+        if (isNegative(a) && isNegative(b))
+            return subtract(b.substr(1), a.substr(1));
+        else if (isNegative(a))
+            return add(b, a.substr(1)).insert(0, "-");
+        else if (isNegative(b))
+            return add(a, b.substr(1));
+
+        if (less(a, b))
+            return subtract(b, a).insert(0, "-");
+        
+        std::string res = "";
+        bool over = false;
+        for (auto it_a = a.rbegin(), it_b = b.rbegin();
+             ;
+             ++it_a, ++it_b) {
+            
+            int r = 0;
+            if (it_a < a.rend())
+                r = (*it_a - '0');
+            else
+                break;
+
+            if (over) {
+                r--;
+                over = false;
+            }
+
+            if (it_b >= b.rend()) {
+                res.insert(0, std::to_string(r));
+                res.insert(0, a.substr(0, std::distance(++it_a, a.rend())));
+                break;
+            }
+
+            r -= (*it_b - '0');
+
+            if (r < 0) {
+                over = true;
+                r += 10;
+            }
+
+            res.insert(0, std::to_string(r));
+        }
+        deleteLeadingZeros(res);
+        return res;
+    }
+
+    //return true if a is negative number, otherwise return false
+    static bool isNegative(std::string a) {
+        return *a.begin() - '0' < 0;
     }
 
     static std::string divide(std::string a, int b) {
--- StringArithmetic/main.cpp
+++ StringArithmetic/main.cpp
@@ -4,13 +4,14 @@
 
 int main() {
     std::string a, b, result;
-
+    std::cout << "String Arithmetic\n";
+    std::cout << "subtract integers, positive and/or negative. (no floats)\n";
     while (1) {
         std::cout << "input a\n";
         std::cin >> a;
         std::cout << "input b\n";
         std::cin >> b;
-        std::cout << a << " + " << b << " = " << StringCalc::add(a, b) << "\n\n";
+        std::cout << a << " - " << b << " = " << StringCalc::subtract(a, b) << "\n\n";
     }
 
     return 0;
